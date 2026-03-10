7cc996488c283114852be519e86dc9e4fafcb1e7
Aythami <aythamimayorsanchez@gmail.com>
2026-03-10 16:56:32 +0100

    Creamos los atributos y el constructor de Nota.java

---
+++ src/Nota.java
@@ -0,0 +1,11 @@
+public class Nota {
+
+    private String titulo;
+    private String contenido;
+
+    public nota(String titulo, String contenido){
+
+        this.titulo = titulo;
+        this.contenido = contenido;
+    }
+}
