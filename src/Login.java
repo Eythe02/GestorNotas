05ea9e143f08ca911a4492c1ef9dd09fa343b30a
Aythami <aythamimayorsanchez@gmail.com>
2026-03-10 18:09:52 +0100

    Creo login y la interfaz básica

--- src/Login.java
+++ src/Login.java
@@ -1,3 +1,37 @@
-public class Login {
-    
+
+    import javax.swing.*;
+
+public class Login extends JFrame {
+    JTextField usuarioField;
+    JPasswordField passField;
+
+    public Login(){
+        setTitle("Login");
+        setSize(300, 200);
+        setLayout(null);
+
+        JLabel l1 = new JLabel("Usuario");
+        l1.setBounds(20,20,100,20);
+        add(l1);
+
+        usuarioField = new JTextField();
+        usuarioField.setBounds(100,20,150,20);
+        add(usuarioField);
+
+        JLabel l2 = new JLabel("Password");
+        l2.setBounds(20,60,100,20);
+        add(l2);
+
+        passField = new JPasswordField();
+        passField.setBounds(100,60,150,20);
+        add(passField);
+
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+    public static void main(String[] args){
+        new Login();
+    }
 }
+
