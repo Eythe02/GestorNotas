e5c61c2688871b7efb15d5c38fa97c8c325b474a
Aythami <aythamimayorsanchez@gmail.com>
2026-03-10 17:55:30 +0100

    Añadimos los campos usuario y contraseña

--- src/Registro.java
+++ src/Registro.java
@@ -8,6 +8,29 @@ public class Registro extends JFrame {
        setSize(300,200);
        setLayout(null);
        setVisible(true);
+
+        JTextField usuario;
+        JPasswordField pass;
+
+        JLabel l1 = new JLabel("Usuario");
+        l1.setBounds(20,20,100,20);
+        add(l1);
+
+        usuario = new JTextField();
+        usuario.setBounds(100,20,150,20);
+        add(usuario);
+
+        JLabel l2 = new JLabel("Password");
+        l2.setBounds(20,60,100,20);
+        add(l2);
+
+        pass = new JPasswordField();
+        pass.setBounds(100,60,150,20);
+        add(pass);
+
+
    }
+
+   
 }
 
